972e7e33e468541fd71626482136bbbed0a4d8cf
Zach <42499219+mathe-matician@users.noreply.github.com>
2019-11-23 20:29:30 -0600

    controls & collisions combined
    need to figure out how to consolidate the player position to tile position conversion code into single check for all.

--- Lumber/controls.asm
+++ Lumber/controls.asm
@@ -1,3 +1,9 @@
+	LDA shadow_oam
+	STA temp_player_y_move
+
+	LDA shadow_oam+3
+	STA temp_player_x_move
+
 MovePlayerUp:	
 	LDA buttons1
 	AND #%00001000
@@ -6,13 +12,49 @@ MovePlayerUp:
 MovingPlayerUp:
 	LDA #$02
 	STA $0201	;character sprite facing away
+	
+	;; check if player has reached left wall
+	LDA $0200
+	CMP #TOPWALL
+	BEQ MovePlayerUpDone
+
+	LDA temp_player_y_move
+	SEC
+	SBC #$08
+	STA temp_player_y_move
+
+	LDA #$00
+	STA L
+	LDA temp_player_y_move
+	AND #%11111000
+	ASL
+	ROL L
+	ASL
+	ROL L
+	ADC #<background_lvl_1_1
+	STA H
+	LDA L
+	ADC #>background_lvl_1_1
+	STA L
+	LDA temp_player_x_move	;should be shadow_oam+3 current location
+	LSR
+	LSR
+	LSR			;divide by 8
+	TAY
+	
+	LDA (H), Y
+	STA fake_player
+	CMP #$04
+	BEQ MovePlayerUpDone
+
+	;; move player up if open space
 	LDA $0200
 	SEC
-	SBC #$01
+	SBC #$08
 	STA $0200
+	JMP MovePlayerUpDone
 	
 MovePlayerUpDone:
-	;; add both feet on ground sprite?
 
 MovePlayerDown:
 	LDA buttons1
@@ -22,13 +64,49 @@ MovePlayerDown:
 MovingPlayerDown:
 	LDA #$01
 	STA $0201
+
+	;; check if player has reached bottom wall
+	LDA $0200
+	CMP #BOTTOMWALL
+	BEQ MovePlayerDownDone
+
+	LDA temp_player_y_move
+	CLC
+	ADC #$08
+	STA temp_player_y_move
+
+	LDA #$00
+	STA L
+	LDA temp_player_y_move
+	AND #%11111000
+	ASL
+	ROL L
+	ASL
+	ROL L
+	ADC #<background_lvl_1_1
+	STA H
+	LDA L
+	ADC #>background_lvl_1_1
+	STA L
+	LDA temp_player_x_move	;should be shadow_oam+3 current location
+	LSR
+	LSR
+	LSR			;divide by 8
+	TAY
+	
+	LDA (H), Y
+	STA fake_player
+	CMP #$04
+	BEQ MovePlayerDownDone
+
+	;; move player down if no collision
 	LDA $0200
 	CLC
-	ADC #$01
+	ADC #$08
 	STA $0200
+	JMP MovePlayerDownDone
 
 MovePlayerDownDone:
-	;; add both feet on ground sprite?
 
 MovePlayerRight:
 	LDA buttons1
@@ -38,10 +116,47 @@ MovePlayerRight:
 MovingPlayerRight:
 	LDA #$03
 	STA $0201
+
+	;; check if player has reached right wall
+	LDA $0203
+	CMP #RIGHTWALL
+	BEQ MovePlayerRightDone
+	
+	LDA temp_player_x_move
+	CLC
+	ADC #$08
+	STA temp_player_x_move
+
+	LDA #$00
+	STA L
+	LDA temp_player_y_move
+	AND #%11111000
+	ASL
+	ROL L
+	ASL
+	ROL L
+	ADC #<background_lvl_1_1
+	STA H
+	LDA L
+	ADC #>background_lvl_1_1
+	STA L
+	LDA temp_player_x_move	;should be shadow_oam+3 current location
+	LSR
+	LSR
+	LSR			;divide by 8
+	TAY
+	
+	LDA (H), Y
+	STA fake_player
+	CMP #$04
+	BEQ MovePlayerRightDone
+
+	;; move player right if no collision
 	LDA $0203
 	CLC
-	ADC #$01
+	ADC #$08
 	STA $0203
+	JMP MovePlayerRightDone
 
 MovePlayerRightDone:
 
@@ -53,10 +168,47 @@ MovePlayerLeft:
 MovingPlayerLeft:
 	LDA #$04
 	STA $0201
+
+	;; check if player has reached left wall
+	LDA $0203
+	CMP #LEFTWALL
+	BEQ MovePlayerLeftDone
+
+	LDA temp_player_x_move
+	SEC
+	SBC #$08
+	STA temp_player_x_move
+
+	LDA #$00
+	STA L
+	LDA temp_player_y_move
+	AND #%11111000
+	ASL
+	ROL L
+	ASL
+	ROL L
+	ADC #<background_lvl_1_1
+	STA H
+	LDA L
+	ADC #>background_lvl_1_1
+	STA L
+	LDA temp_player_x_move	;should be shadow_oam+3 current location
+	LSR
+	LSR
+	LSR			;divide by 8
+	TAY
+	
+	LDA (H), Y
+	STA fake_player
+	CMP #$04
+	BEQ MovePlayerLeftDone
+
+	;; move player left if no collision
 	LDA $0203
 	SEC
-	SBC #$01
+	SBC #$08
 	STA $0203
+	JMP MovePlayerLeftDone
 
 MovePlayerLeftDone:	
 
