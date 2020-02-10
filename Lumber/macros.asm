b03e69982ffc62441798630abf7729ddf0f6415e
Zach <42499219+mathe-matician@users.noreply.github.com>
2020-02-10 15:51:04 -0600

    updated macros

--- Lumber/macros.asm
+++ Lumber/macros.asm
@@ -1,18 +1,72 @@
 	.macro PushAll
-	PHP
-	PHA
-	TXA
-	PHA
-	TYA
-	PHA
+		PHP
+		PHA
+		TXA
+		PHA
+		TYA
+		PHA
 	.endm
 
 	.macro PullAll
-	PLA
-	TAY
-	PLA
-	TAX
-	PLA
-	PLP
+		PLA
+		TAY
+		PLA
+		TAX
+		PLA
+		PLP
 	.endm
+
+	MACRO TileTranslate Reg1, Reg2, spritey, spritex, buffer
+		LDA #$00
+		STA Reg2
+		LDA spritey
+		AND #%11111000
+		ASL
+		ROL Reg2
+		ASL
+		ROL Reg2
+		ADC #<buffer
+		STA Reg1
+		LDA Reg2
+		ADC #>buffer
+		STA Reg2
+		LDA spritex
+		LSR
+		LSR
+		LSR	
+		TAY
+		LDA (Reg1), Y
+	ENDM
+
+	MACRO PlayerDeathCheck enemyy, enemyx, playery, playerx
+		LDA enemyy
+		CMP playery
+		BNE Abso_Done
+		
+		LDA enemyx
+		CMP playerx
+		BNE Abso_Done
+		
+		LDA #$03
+		STA shadow_oam+2
+Abso_Done:	
+		;; LDA %00000010
+		;; STA STATE
+		
+	ENDM
+
+	MACRO PRNG S1
+		LDY #$08		;iteration count (generates 8 bits)
+		LDA S1+0
+Seed1:
+		ASL
+		ROL S1+1
+		BCC Seed2
+		EOR #$39		;apply XOR feedback whenever a 1 bit is shifted out
+Seed2:
+		DEY
+		BNE Seed1
+		STA S1+0
+		CMP #$00		;reload flags
+	ENDM
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
