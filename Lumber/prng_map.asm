0b62c7ed7be3870f94f7714025b4166d159a8ea2
Zach Mathe <42499219+mathe-matician@users.noreply.github.com>
2020-03-24 22:35:36 -0500

    75% random map

--- Lumber/prng_map.asm
+++ Lumber/prng_map.asm
@@ -4,17 +4,80 @@ vblankwait2:
 
 	LDA #%10010000
 	STA $2000
-	LDA #%00000000
+	LDA #$00
 	STA $2001
 
+;; 	LDA #$00
+;; 	STA BG256
+;; 	STA PRGtest+0
+;; 	LDA #$3C
+;; 	STA PRGtest+1
+;; 	LDA #$04
+;; 	STA BGCount
+;; PRNG_MAP:	
+;; 	LDY #$08		;iteration count (generates 8 bits)
+;; 	LDA seed2+0
+;; One:
+;; 	ASL
+;; 	ROL seed2+1
+;; 	BCC Two
+;; 	EOR #$39		;apply XOR feedback whenever a 1 bit is shifted out
+;; Two:
+;; 	DEY
+;; 	BNE One
+;; 	STA seed2+0
+;; 	CMP #$00		;reload flags
+
+;; 	LDA seed2
+;; 	CMP #$0C
+;; 	BCC LoadRock		; if seed2 < 12:
+;; 	CMP #$25
+;; 	BCC LoadTree		;if seed2 < 37 and it is greater than 12
+;; 	CMP #$CF
+;; 	BCC LoadGrass		;if seed2 < 207 and is greater than 37
+;; 	CMP #$FF
+;; 	BCC LoadTree		;if seed2 < 255 and is greater than 207
+;; 	JMP BGDone
+;; LoadRock:
+;; 	LDA #$3B
+;; 	STA PRGROM, x
+;; 	JMP LoadCycle
+;; LoadGrass:
+;; 	LDA #$00
+;; 	STA PRGROM, x
+;; 	JMP LoadCycle
+;; LoadTree:	
+;; 	LDA #$04
+;; 	STA PRGROM, x	
+;; LoadCycle:
+;; 	INX
+;; 	BNE PRNG_MAP
+;; 	DEC BGCount
+;; 	BEQ BGDone
+;; 	JMP PRNG_MAP
+;; BGDone:
+;;//////////////////////////////////////////////////
 	LDA #$00
-	STA BG256
+	STA Point+0
+	STA Point+1
+BGTORAM:
+	;; put the map index  in register A 
+	LDA $0000	     ;might need to be just zero
+	;; multiply index by 2, since it is 16bit
+	ASL
+	TAY
+
+	LDA bg_point_ram+0,y
+	STA Point+0
+	LDA bg_point_ram+1,y
+	STA Point+1
+	;; copy 1024 bytes from the location indicated by the pointer to VRAM
 	LDA #$04
-	STA BGCount
-	LDA #$23
-	STA seed2+0
-	LDA #$60
-	STA seed2+1
+	STA BG256
+	LDX #$00
+	LDY #$00
+	TYA
+	PHA
 PRNG_MAP:	
 	LDY #$08		;iteration count (generates 8 bits)
 	LDA seed2+0
@@ -40,24 +103,57 @@ Two:
 	BCC LoadTree		;if seed2 < 255 and is greater than 207
 	JMP BGDone
 LoadRock:
+	PLA
+	TAY
 	LDA #$3B
-	STA PRGROM, x
+	STA (Point), y
 	JMP LoadCycle
 LoadGrass:
+	PLA
+	TAY
 	LDA #$00
-	STA PRGROM, x
+	STA (Point), y
 	JMP LoadCycle
-LoadTree:	
+LoadTree:
+	PLA
+	TAY
 	LDA #$04
-	STA PRGROM, x
+	STA (Point), y
 LoadCycle:
+	INY
+	TYA
+	PHA
 	INX
 	BNE PRNG_MAP
-	DEC BGCount
-	BEQ BGDone
-	JMP PRNG_MAP
-BGDone:
+	INC Point+1
+	LDA BG256
+	SEC
+	SBC #$01
+	STA BG256
+	BNE PRNG_MAP
+BGDone:	
 
+;; bbbCBLoop:	
+;; 	LDA (Point), y
+;; 	STA $6000
+;; 	INC $6000
+;; 	DEX
+;; 	BNE bbbCBLoop
+;; 	INY
+;; 	BNE bbbCopyByte
+;; 	INC Point+1
+;; 	LDA BG256
+;; 	SEC
+;; 	SBC #$01
+;; 	STA BG256
+;; 	BNE bbbCopyByte
+;;/////////////////////////////////////////////////
+LoadBackground:
+	LDA $2002            
+	LDA #$20
+	STA $2006           
+	LDA #$00
+	STA $2006
 		;; put the map index (nametable address) in register A going into this
 	LDA $2000
 	;; multiply index by 2, since it is 16bit
@@ -74,7 +170,7 @@ BGDone:
 	LDA bg_point_ram+1, y
 	STA Pointer+1
 	;; copy 1024 bytes from the location indicated by the pointer to VRAM
-	LDA #$01
+	LDA #$04
 	STA BG256
 	LDX #$00
 	LDY #$00
@@ -82,7 +178,7 @@ CopyByte:
 	LDA (Pointer),y
 	STA $2007
 	INY
-	DEX
+	INX
 	BNE CopyByte
 	INC Pointer+1
 	LDA BG256
