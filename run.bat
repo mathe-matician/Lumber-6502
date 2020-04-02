54d68cae9386c81fd5d3e296cbd36d59b7e696ee
Zach Mathe <42499219+mathe-matician@users.noreply.github.com>
2020-04-02 09:44:18 -0500

    choose to recompile & run or run

--- run.bat
+++ run.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
+:: Enter any argument to recompile
+IF "%1"=="" GOTO Run
+ELSE
 :: Recompile main
 asm6 main.asm Lumber.nes
 echo =============
 echo Recompiled...
+:Run
 echo =============
 echo Running...
 FCEUX Lumber.nes
